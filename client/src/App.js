be714c120f075932007e83c4c9de6c051271e64e
Jonathan Rein <jrein1296@gmail.com>
2021-09-21 13:53:06 -0400

    Adding Home and Navbar components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from "react-redux";
-import memories from "./images/memories.png";
-import Posts from "./components/Posts/Posts.js";
-import Form from "./components/Form/Form.js";
-import useStyles from "./styles.js";
-import { getPosts } from "./actions/posts.js";
+import React from "react";
+import { Container } from "@material-ui/core";
+import Navbar from "./components/Navbar/Navbar.js";
+import Home from "./components/Home/Home.js";
 
 const App = () => {
-  // useState(null) stats with null if there is no id.
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  // useEffect will dispatch getPosts when currentID changes, or when any function is dispatched.
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
 
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h3" align="center">
-          MY POSTS
-        </Typography>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid
-            className={classes.mainContainer}
-            container
-            justifyContent="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} md={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+    <Navbar />
+    <Home />
     </Container>
   );
 };
